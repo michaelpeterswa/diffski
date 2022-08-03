f0eec43ef5c9713e1b1d90595e870d57a5ae0ddb
Michael Peters <michaelpeterswa@icloud.com>
2022-08-02 22:20:38 -0700

    fix: add everything else

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,37 @@
+use similar::{ChangeTag, TextDiff};
 use wasm_bindgen::prelude::*;
 
 // simple addition function to test WASM
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn diff(a: &str, b: &str) -> String {
+    let diff = TextDiff::from_lines(a, b);
+
+    let mut diffs: Vec<String> = Vec::new();
+    for change in diff.iter_all_changes() {
+        let sign = match change.tag() {
+            ChangeTag::Delete => "-",
+            ChangeTag::Insert => "+",
+            ChangeTag::Equal => " ",
+        };
+        diffs.push(format!("{}{}", sign, change));
+    }
+
+    return diffs.join("");
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::add;
+    use crate::diff;
 
     #[test]
-    fn test_add() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_diff_mismatch() {
+        let result = diff("hello\ngoodbye", "hello\nworld");
+        assert_eq!(result, " hello\n-goodbye\n+world\n");
+    }
+
+    #[test]
+    fn test_diff_match() {
+        let result = diff("hello\ngoodbye", "hello\ngoodbye");
+        assert_eq!(result, " hello\n goodbye\n");
     }
 }
